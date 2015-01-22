551ceaf80dfb7cb5227090934d1f9949f9e93d9b
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-21 20:24:46 -0800

    Docs: Adds comments rel #1

--- lab/index.js
+++ lab/index.js
@@ -2,29 +2,42 @@
 
 (function(global){
 
+    /**
+     * Injects Google API Javascript File and adds a callback to load the Google Maps Async
+     * @type {{load: Function}}
+     * @private
+     */
     var _googleMapsApi = {
         load: function(args) {
-
             var version = args.version || '3.16';
-
             var script = document.createElement('script');
             script.type = 'text/javascript';
             script.src = '//maps.googleapis.com/maps/api/js?v=' + version + '&' +
             'callback=GMP.maps.' + args.id + '.create';
-            global.document.body.appendChild(script);
+            return global.document.body.appendChild(script);
         }
     };
 
+    /**
+     * Creates a new Google Map Instance
+     * @param args
+     */
     function newMap(args)
     {
         var mapOptions = {
             zoom: args.zoom,
             center: new global.google.maps.LatLng(args.center.lat, args.center.lng)
         };
-        global.GMP.maps[args.id].map = new global.google.maps.Map(document.getElementById(args.id), mapOptions);
+
+        return global.GMP.maps[args.id].map = new global.google.maps.Map(document.getElementById(args.id), mapOptions);
 
     }
 
+    /**
+     * Creates a new GMaps Plus instance
+     * @param options
+     * @constructor
+     */
     function GMP(options)
     {
         if (typeof options == 'object') {
