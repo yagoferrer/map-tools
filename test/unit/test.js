804d47effb9e0a7dfcf23a513681d01da2b6f95a
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-23 17:38:57 -0800

    Tests: for adding Markers #16

--- test/unit/test.js
+++ test/unit/test.js
@@ -6,7 +6,7 @@ describe('Given GMPlus', function () {
 
 
     it('should return an error if you don\'t pass a options parameter', function (done) {
-      GMP(null, function (err) {
+      var map = new GMP(null, function (err) {
         expect(err.message).to.equals('You must pass a valid first parameter: options');
         done();
       });
@@ -14,7 +14,7 @@ describe('Given GMPlus', function () {
 
 
     it('should return an error if you don\'t pass a valid "id" or "class" property value', function (done) {
-      GMP({}, function(err) {
+      var map = new GMP({}, function(err) {
         expect(err.message).to.equals('You must pass an "id" or a "class" property values');
         done();
       });
@@ -22,19 +22,43 @@ describe('Given GMPlus', function () {
 
 
     it('should return an error if you don\'t pass a valid "lat" or "lng" values', function (done) {
-      GMP({id:'myMap'}, function(err) {
+      var map = new GMP({id:'myMap'}, function(err) {
         expect(err.message).to.equals('You must pass valid "lat" (latitude) and "lng" (longitude) values');
         done();
       });
     });
 
     it('should return a Map instance when you pass: "id", "lat" and "lng"', function (done) {
-      GMP({async: false, id: 'myMap', lat: '41.3833', lng: '2.1833'}, function(err, instance) {
+      var map = new GMP({async: false, id: 'myMap', lat: 41.3833, lng: 2.1833}, function(err, instance) {
         expect(instance.gm_bindings_).to.be.a('object');
         done();
-
       });
+    });
+  });
+
+
+  describe('when calling addMarker', function () {
+
+    it('should add Markers to the Map', function() {
+      var map = new GMP({async: false, id: 'myMap', lat: 41.3833, lng: 2.1833}, function(){});
+
+      var result = map.addMarker([{
+          lat: 41.3833,
+          lng: 2.1833,
+          title: 'barcelona'
+        },
+          {
+            lat: 41.4489,
+            lng: 2.2461,
+            title: 'badalona'
+          }],
+        {group: 'myGroup'});
+
+
+      expect(result).to.have.length.of(2);
+      expect(GMP.maps.myMap.groups.myGroup).to.have.length.of(2);
     });
 
   });
+
 });
