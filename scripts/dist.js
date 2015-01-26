12a2ef4ef80052778384daa00fa3811214c325d2
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-25 17:38:18 -0800

    Adds script to minify files for distribution closes: #60

---
+++ scripts/dist.js
@@ -0,0 +1,23 @@
+var UglifyJS = require('uglify-js');
+var fs = require('fs');
+var file = 'lib/index.js';
+var result = UglifyJS.minify(file, {
+  mangle: true,
+  compress: {
+    sequences: true,
+    dead_code: true,
+    conditionals: true,
+    booleans: true,
+    unused: true,
+    if_return: true,
+    join_vars: true,
+    drop_console: true
+  }
+});
+
+var pjson = require('../package.json');
+var credits = '/* GMPlus.js ' + pjson.version + ' MIT License. ' + new Date().getFullYear() + ' Yago Ferrer <yago.ferrer@gmail.com> */\n';
+fs.writeFileSync('dist/gmplus.min.js', credits + result.code);
+var data = fs.readFileSync(file, 'utf-8');
+fs.writeFileSync('dist/gmplus.js', credits + data, 'utf-8');
+
