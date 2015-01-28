22e2fd724b8900aee5e4a0b301358fb1d08fd302
Yago Ferrer <yferrer@zephyrhealth.com>
2015-01-28 12:24:22 -0800

    Tests: adds code coverage and fixes a bug! rel: #74

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,7 +1,11 @@
 'use strict';
 
 var chai = require('chai');
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
 global.expect = chai.expect;
+
+global.sinon = require('sinon');
 
 var window = require('../test/mocks/window');
 window.google = require('../test/mocks/google-maps');
@@ -13,3 +17,6 @@ global.GMP = window.GMP;
 
 // To test independent modules.
 global.window = window;
+
+
+
