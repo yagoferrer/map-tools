f6bdba49a6d842561afca39b396bf6d75ef41203
Yago Ferrer <yago.ferrer@gmail.com>
2015-02-08 10:35:46 -0800

    Updates 'myMap' reference to 'mymap'

--- dist/gmplus.js
+++ dist/gmplus.js
@@ -227,7 +227,7 @@ module.exports = function (global) {
 /*jslint node: true */
 "use strict";
 module.exports = {
-  version: '3.exp',
+  version: '3.18', // Released: May 15, 2014
   zoom: 8,
   customMapOptions: ['id', 'lat', 'lng', 'type'],
   customMarkerOptions: ['lat', 'lng', 'move', 'bubble']
