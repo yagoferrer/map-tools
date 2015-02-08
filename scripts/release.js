2671ba5a9e23f3e88122ff767470156bf21d088a
Yago Ferrer <yago.ferrer@gmail.com>
2015-02-08 10:17:00 -0800

    Build: Create a script that updates files for a new release. #92

---
+++ scripts/release.js
@@ -0,0 +1,41 @@
+'use strict';
+
+var current = new RegExp(/0.2.\d+/);
+var next = '0.3.0';
+var description = "Gmplus.js is a Google Maps Feature-rich Javascript wrapper that makes things like: Marker filtering, asynchronous loading, working with TopoJSON or GeoJSON, animation and more. Much simpler with an easy-to-use API.";
+
+var fs = require('fs');
+
+
+function replaceDocs(location, current, next) {
+  fs.readFile(location, 'utf8', function (err, data) {
+    if (err) {return console.log(err);}
+    var result = data.replace(current, next);
+    fs.writeFile(location, result, 'utf8', function (err) {
+      if (err) return console.log(err);
+    });
+  });
+}
+
+
+function replaceJson(location, destination, next) {
+  var json = require(location);
+  json.version = next;
+  json.description = description;
+
+  var output = JSON.stringify(json, null, "  ");
+
+
+  fs.writeFile(destination, output, 'utf8', function (err) {
+    if (err) return console.log(err);
+  });
+
+
+}
+
+replaceDocs('./README.md', current, next);
+replaceJson('../package.json', './package.json', next);
+replaceJson('../bower.json', './bower.json', next);
+
+
+
