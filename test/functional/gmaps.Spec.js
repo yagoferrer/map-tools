68238e8d2bf9881a32b170561df8fe96f2c4142d
Yago Ferrer <yago.ferrer@gmail.com>
2015-02-14 12:00:21 -0800

    (feature) Make crossfilter dimensions even easier to use. Fixes #136

--- test/functional/gmaps.Spec.js
+++ test/functional/gmaps.Spec.js
@@ -46,7 +46,7 @@ describe('Given gmplus.js', function () {
 
       it('should add the marker data to the filters', function () {
         var spy = sinon.spy();
-        var map = new GMP({sync: true, id: 'mymap', type: 'ROADMAP', lat: 41.3833, lng: 2.1833, filters: {add: spy}});
+        var map = new GMP({sync: true, id: 'mymap', type: 'ROADMAP', lat: 41.3833, lng: 2.1833, indexes: [{add: spy}]});
 
         var result = map.addMarker({
           lat: 42.5000,
