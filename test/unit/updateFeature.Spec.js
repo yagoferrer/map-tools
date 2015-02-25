1c0d9b88502d685907942b9944c169bc5e13c041
Yago Ferrer <yferrer@zephyrhealth.com>
2015-02-25 05:56:48 -0800

    Add crossfilter support for GeoJSON features fixes: #130

--- test/unit/updateFeature.Spec.js
+++ test/unit/updateFeature.Spec.js
@@ -5,64 +5,41 @@ describe('Given the updateFeature Module', function() {
       GMP: {
         maps: {mymap: {}}}
     };
-
     that = {
       id: 'mymap',
       instance : {
         data :{/* overrideStyle mock*/}
       }
     };
-
   });
+
 
   it('should update a Feature', function() {
-
     var spy = sinon.spy();
-
     that.instance.data.overrideStyle = spy;
-
     var updateFeature = require('map-tools/updateFeature')(global, that);
-
-    updateFeature({uid:1}, {style: {fillColor: '#ffffff'}});
-
-    expect(spy).to.have.been.calledWith({uid:1},{fillColor: '#ffffff'});
-
+    updateFeature({data: {uid:1}}, {style: {fillColor: '#ffffff'}});
+    expect(spy).to.have.been.calledWith({data: {uid:1}},{fillColor: '#ffffff'});
   });
 
-   it('should update an array of Features', function() {
 
+  it('should update an array of Features', function() {
     var spy = sinon.spy();
-
     that.instance.data.overrideStyle = spy;
-
     var updateFeature = require('map-tools/updateFeature')(global, that);
-
-    updateFeature([{uid:1}, {uid:2}], {style: {fillColor: '#ffffff'}});
-
+    updateFeature([{data: {uid:1}}, {data: {uid:2}}], {style: {fillColor: '#ffffff'}});
     expect(spy).to.have.been.calledTwice;
-
-    expect(spy.firstCall.args).to.eql([{uid:1}, {fillColor: '#ffffff'}]);
-
-    expect(spy.secondCall.args).to.eql([{uid:2}, {fillColor: '#ffffff'}]);
-
+    expect(spy.firstCall.args).to.eql([{data: {uid:1}}, {fillColor: '#ffffff'}]);
+    expect(spy.secondCall.args).to.eql([{data: {uid:2}}, {fillColor: '#ffffff'}]);
   });
+
 
 	it('should update the Features based on the styling function', function() {
-
 		var spy = sinon.spy();
 		var spyStyling = sinon.spy();
-
 		that.instance.data.overrideStyle = spy;
-
 		var updateFeature = require('map-tools/updateFeature')(global, that);
-
-		updateFeature({uid:1}, {style: spyStyling});
-
+		updateFeature({data: {uid:1}}, {style: spyStyling});
 		expect(spyStyling).to.have.been.called;
-
-
 	});
-
-
-
 });
