88d8596b8a7ac6a078f6bfe6d4c8525f77005376
Yago Ferrer <yago.ferrer@gmail.com>
2015-04-25 09:21:39 -0700

    Refactor: the the bootstrap file and replace global for window and fix tests. #156

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,22 +1,17 @@
-'use strict';
+"use strict";
 
+// Need it for testing
 var chai = require('chai');
 var sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 global.expect = chai.expect;
-
 global.sinon = require('sinon');
+
+// Dependencies
+global.crossfilter = require('crossfilter');
 
 var window = require('../test/mocks/window');
 window.google = require('../test/mocks/google-maps');
 
-var mapTools = require('map-tools/index')(window);
-window.mapTools = mapTools;
-global.mapTools = window.mapTools;
-
-
-// To test independent modules.
+window.mapTools = global.mapTools = require('map-tools/index')(window);
 global.window = window;
-global.google = window.google;
-
-global.crossfilter = require('crossfilter');
